50e0a74c7f8c516bcbe883c1a61a5853b6fdfdcf
Reza Ghiyats Fikri <141542911+SUPERREZZ@users.noreply.github.com>
2025-07-15 11:23:10 +0700

    Update db.js

--- coba Fullstack/src/config/db.js
+++ coba Fullstack/src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql")
 const util = require("util")
 
 const db = mysql.createConnection({
-    host: "a3t.h.filess.io",
-    user: "FullStackJs_doingshape",
-    password: "a9dcea8354178c347a42dbc0c8f5d2cb4ea0cd9f",
-    database: "FullStackJs_doingshape"
+    host: "5lty1a.h.filess.io",
+    user: "fullstack_wheresnow",
+    password: "bca116f00b52d5c6b89cd4773dd13a612a6e2c6a",
+    database: "fullstack_wheresnow"
 })
 db.connect(err => {
     err ? console.error(err) : console.log("Database connected")
